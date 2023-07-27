8de26755c8fd22cae36ebe8e19ef66324ac872ec
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-07-26 19:07:29 -0500

    step 24

---
+++ AG Meany Bird/Assets/Scripts/Move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Header("Default Speed")]
+public float speed;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
